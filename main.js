eaedd9ffc47fd821c208a112e476da9ec8e3c410
veronicasilva20 <santinoveronicasilva@gmail.com>
2023-05-17 17:27:02 -0300

    practica de javascript

---
+++ main.js
@@ -0,0 +1,125 @@
+var miVariable = 44;
+console.log('mi edad es 44');
+
+var miVariable = 12;
+// console.log('mi edad es 12');
+
+const miconstante = 3;
+// console.log('el valor de mi constante es' +miconstante);
+
+var resultado = 2+2;
+// console.log('el resultado es :' +resultado);
+
+var op1=1;
+var op2=2;
+var resultado=op1+op2;
+console.log('resultado es:'+ resultado);
+
+//condicionales
+
+let numero = 6 ;
+
+let nombre = 'lucia' ;
+// let resultadopregunta = miNumero == 7
+// console.log(resultadopregunta);
+
+// if (nombre ==='marta'){
+//     console.log('si');
+// }else{
+//     console.log('no');
+// }
+
+
+
+// if (numero == 7){
+//     console.log('si');
+    
+// }else{
+//     console.log('no');
+
+// }
+
+// let minumero2= 0;
+// if (minumero2>0){
+//     console.log('mi numero es positivo');
+// }else if (minumero2 ===0){
+// console.log('mi numero es zearo');
+// }else{
+//     console.log('mi nuemro es negativo');
+// }
+
+
+// let contador = 0
+// while (contador <20);{
+// console.log('contador');
+// contador = contador + 1;
+// }
+
+// for(let contador=0; contador < 10; contador = contador + 1){
+//     console.log(contador);
+// }
+    
+function saludar(nombre,edad){
+    console.log('hola,mi nombre es:'+ nombre);
+    console.log('mi edad es:'+ edad);
+}
+function multiplicar(num1,mun2){
+    let resultados= num1 * num2;
+    return resultados;
+}
+// console.log(resultados);
+
+// arreglos o arrays
+
+let miarreglo=['raul','alberto','franco'];
+for (let i= 0; i < 4; i ++ );
+{
+    console.log('accediendo al indice:' + i);
+    let mostrar = miarreglo[i];
+    console.log(mostar);
+};
+//objetos
+let persona = {
+    nombre:'raul',
+    edad:45,
+    masculino:true,
+};
+let perasona1 = {
+    nombre:'rafael',
+    edad:34,
+    masculino:false,
+
+}
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
